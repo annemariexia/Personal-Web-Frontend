1262abd1668b322d467a91d646c215f2fd608c3e
Annemarie Xia <xx2410@columbia.edu>
2024-03-18 14:42:50 -0400

    create landing title

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import "../styles/LandingPage.css"
+
+const LandingPage = () => {
+    return (
+        <>
+            <h1>Hi, I'm Annemarie.</h1>
+            <h2 className="typed-out">I'm a fullstack developer, climber, and coffee connoisseur.</h2>
+        </>
+    )
+}
+
+export default LandingPage;
