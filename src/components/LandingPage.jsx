4ccfee6a6257aee4efd46839f4248e871518672d
Annemarie Xia <xx2410@columbia.edu>
2024-03-18 22:42:38 -0400

    finish landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -7,12 +7,13 @@ import Github from "../assets/github-icon.svg"
 const LandingPage = () => {
     return (
         <div className="landing-page">
+            
             <h1 className="text">Hi, I'm Annemarie.</h1>
             <h3 className="typed-out">I'm a fullstack developer, climber, and coffee connoisseur.</h3>
             <div className="icon-box">
-                <img src={Linkedin} alt="Linkedin" className="linkedin-icon" />
-                <img src={Instagram} alt="Instagram" className="instagram-icon" />
-                <img src={Github} alt="Github" className="github-icon" />
+                <a href="https://www.linkedin.com/in/annemarie-x-xia/"><img src={Linkedin} alt="Linkedin" className="linkedin-icon" /></a>
+                <a href="https://www.instagram.com/anne_climbs/"><img src={Instagram} alt="Instagram" className="instagram-icon" /></a>
+                <a href="https://github.com/annemariexia"><img src={Github} alt="Github" className="github-icon" /></a>
             </div>
             <img src={Arrow} alt="Arrow" className="arrow-icon" />
         </div>
