48df0da77f86b2aa2a510cf9728c5e5a6d2f2a68
Annemarie Xia <xx2410@columbia.edu>
2024-03-21 19:08:45 -0400

    add responsive design

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -10,14 +10,9 @@ const LandingPage = () => {
                 <img src={Photo} className="photo"/>
             </div>
             <div className="text">
-                <h1 className="slide-in">Hi, I'm Annemarie.</h1>
+                <h1 className="slide-in">"Hi, I'm Annemarie."</h1>
                 <h3 className="typed-out">I'm a fullstack developer, climber, and coffee connoisseur.</h3>
             </div>
-            {/* <div className="icon-box">
-                <a href="https://www.linkedin.com/in/annemarie-x-xia/"><img src={Linkedin} alt="Linkedin" className="linkedin-icon" /></a>
-                <a href="https://www.instagram.com/anne_climbs/"><img src={Instagram} alt="Instagram" className="instagram-icon" /></a>
-                <a href="https://github.com/annemariexia"><img src={Github} alt="Github" className="github-icon" /></a>
-            </div> */}
             <div className="arrow">
                 <img src={Arrow} alt="Arrow" className="arrow-icon" />
             </div>
