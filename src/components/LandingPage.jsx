d7befa1ad867bb6bb6dd7ceb3b5507231407406e
Annemarie Xia <xx2410@columbia.edu>
2024-03-22 15:00:41 -0400

    add blur effect to landing page when scrolling

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,31 @@
 import "../styles/LandingPage.css"
 import Arrow from "../assets/arrow-down.svg"
-
 import Photo from "../assets/head-shot.jpeg"
+import React, { useState, useEffect } from 'react';
+
 
 const LandingPage = () => {
+
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [lastScrollTop, setLastScrollTop] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = window.scrollY;  // number of pixels measured from vertical scroll
+
+            setIsScrolled(scrollTop > lastScrollTop);
+            setLastScrollTop(scrollTop <= 0 ? 0 : scrollTop);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [lastScrollTop])
+
     return (
-        <div className="landing-page">
+        <div className={`landing-page ${isScrolled ? 'blur' : ''}`}>
             <div className="photo">
                 <img src={Photo} className="photo"/>
             </div>
