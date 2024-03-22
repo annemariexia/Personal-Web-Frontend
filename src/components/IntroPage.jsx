f65c0f3f4e2180f5d3aab5ca24f2abe02b7f8f32
Annemarie Xia <xx2410@columbia.edu>
2024-03-21 23:40:19 -0400

    finish redesigning intro page section 1

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -12,7 +12,20 @@ const IntroPage = () => {
             <section className="education-background">
               <p>I graduated from UC Davis with a BS in Computer Science and Cognitive Science. </p>
               <p>Delving into the intricacies of the human mind while navigating the dynamic world of technology has enabled me to approach problems from multiple angles, blending analytical thinking with an understanding of human-centered design principles.</p>
-              <p>Upon graduating from college, I recoginized the transformative potential of machine learning decided to continue my academic journey at Columbia University.</p>
+              <p>Upon graduating from college, I recognized the transformative potential of machine learning decided to continue my academic journey at Columbia University.</p>
+              <p>As a fullstack developer, I am driven to bridge the gap between technology and human needs. I believe that by embracing AI technologies and understanding user behavior and psychology, I can create innovative solutions that positively impact people's lives.</p>
+            </section>
+          </div>
+
+          <div className="photo-box">
+            <img src={GradHeadShot} alt="grad-photo" className="grad-photo" />
+          </div>
+          <div className="text-box">
+            <h3 className="subtitle">CS and CS: Harmonizing Tech and Thought</h3>
+            <section className="education-background">
+              <p>I graduated from UC Davis with a BS in Computer Science and Cognitive Science. </p>
+              <p>Delving into the intricacies of the human mind while navigating the dynamic world of technology has enabled me to approach problems from multiple angles, blending analytical thinking with an understanding of human-centered design principles.</p>
+              <p>Upon graduating from college, I recognized the transformative potential of machine learning decided to continue my academic journey at Columbia University.</p>
               <p>As a fullstack developer, I am driven to bridge the gap between technology and human needs. I believe that by embracing AI technologies and understanding user behavior and psychology, I can create innovative solutions that positively impact people's lives.</p>
             </section>
           </div>
